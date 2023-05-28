6b05e344b9ce17b408e663ac5d2f60214687e273
akashpratap0703 <akashpratap0703@gmail.com>
2023-05-28 13:08:56 +0530

    form Integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import '../css/contact.css' 
 export default function Contact() {
-  const submitform= ()=>{}
-// var scriptURL = 'https://script.google.com/macros/s/AKfycbyEnMW6vdeaBZ5iFEQ08OSLmwhRhfY_GlKYa5ndBF5NIWC_mCr1piQLg9Qhzbh2U6geIQ/exec'
-// var form = document.forms['submit-to-google-sheet']
-// var msg = document.getElementById("msg")
-// form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//     .then(response => {
-//         msg.innerHTML = "Message sent successfully"
-//         setTimeout(function(){
-//             msg.innerHTML = ""
-//         },5000)
-//         form.reset()
-//     })
-//     .catch(error => console.error('Error!', error.message))
-// })
+const submitform=function(e){
+var scriptURL = 'https://script.google.com/macros/s/AKfycbyEnMW6vdeaBZ5iFEQ08OSLmwhRhfY_GlKYa5ndBF5NIWC_mCr1piQLg9Qhzbh2U6geIQ/exec'
+var form = document.forms['submit-to-google-sheet']
+var msg = document.getElementById("msg")
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        form.reset()
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        
+    })
+    .catch(error => console.error('Error!', error.message))
+}
   return (
     <div>
       <div id='contact'>
@@ -27,10 +27,10 @@ export default function Contact() {
                     <p><i className='fas fa-paper-plane'></i>akashpratap0703@gmail.com</p>
                     <p><i className='fas fa-phone-square-alt '></i>8299821512</p>
                     <div className='social-icons'>
-                        <a href="/"><i className='fab fa-facebook'></i></a>
+                        <a href="https://leetcode.com/akashpratap0703/" target="_blank" rel="noreferrer" title="leetcode"><i className="fa-solid fa-laptop-code"></i></a>
                         <a href="/"><i className='fab fa-twitter-square'></i></a>
-                        <a href="https://www.instagram.com/pr_tapakash/" target="_blank" rel="noreferrer"><i className='fab fa-instagram'></i></a>
-                        <a href="https://www.linkedin.com/in/akash-pratap-599006202/" target="_blank" rel="noreferrer"><i className='fab fa-linkedin'></i></a>
+                        <a href="https://www.instagram.com/pr_tapakash/" target="_blank" rel="noreferrer" title="instagram"><i className='fab fa-instagram'></i></a>
+                        <a href="https://www.linkedin.com/in/akash-pratap-599006202/" target="_blank" rel="noreferrer" title="linkedin"><i className='fab fa-linkedin'></i></a>
                     </div>
                     <a href="../images/Resume.pdf" download className='btn btn2'>Download Resume</a>
                 </div>
@@ -39,8 +39,9 @@ export default function Contact() {
                         <input type='text' name='Name' placeholder='Your Name' required />
                         <input type='email' name='Email' placeholder='Your Email' required />
                         <textarea name='Message' rows='6' placeholder='Your Message' required />
-                        <button type='submit' className='btn btn2' onClick={()=>submitform()}>Submit</button>
+                        <button type='submit' className='btn btn2' onClick={(event)=>submitform(event)}>Submit</button>
                     </form>
+                    
                     <span id="msg"></span>
                 </div>
             </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,31 @@
-  import React from 'react'
+  import React,{useState} from 'react'
   import '../css/navbar.css';
-
+  import {FaArrowCircleUp} from 'react-icons/fa';
+  import { Button } from './Styles';
 
   export default function NavBar() {  
+
+    const [visible, setVisible] = useState(false)
+  
+      const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300){
+          setVisible(true)
+        } 
+        else if (scrolled <= 300){
+          setVisible(false)
+        }
+      };
+      
+      const scrollToTop = () =>{
+        window.scrollTo({
+          top: 0, 
+          behavior: 'smooth'
+          /* you can also use 'auto' behaviour
+            in place of 'smooth' */
+        });
+      }; 
+    window.addEventListener('scroll', toggleVisible);
     const openmenu= ()=>{
       var sidemenu = document.getElementById("sidemenu");
       sidemenu.style.right = "0"
@@ -11,6 +34,7 @@
       var sidemenu = document.getElementById("sidemenu");
       sidemenu.style.right = "-200px"
     }
+    
     return (
       <div>
       <div className="container-nav">
@@ -22,6 +46,10 @@
               <li><a href="#projects">Projects</a></li>
               <li><a href="#journey">Journey</a></li>
               <li><a href="#contact">Contact</a></li>
+              <Button title="scroll to top">
+                <FaArrowCircleUp onClick={scrollToTop} 
+                style={{display: visible ? 'inline' : 'none' ,align:'left'}} />
+              </Button>
               <i className='fas fa-times' onClick={()=>closemenu()}></i>
           </ul>
           <i className='fas fa-bars' onClick={()=>openmenu()}></i>
